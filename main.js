fa62e08d5ef24ba3dfbe0636c8fb7c578993d0fe
ncruz93 <nickolas438@gmail.com>
2022-12-13 02:06:05 -0500

    added missed page to nav

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ createApp({
         return {
           items: [
             { text: 'Home Page', link: "index.html" },
-            { text: 'Gallery', link:"gallery.html"},
+            { text: 'Dogs Page', link:"dogs.html"},
             { text: 'Hobbies Page', link: "hobbies.html" },
+            { text: 'Gallery', link:"gallery.html"},
             { text: 'Bio', link:"bio.html"},
-            { text: 'Contact Me', link:"contact.html"}
+            { text: 'Contact Me', link:"contact.html"},
         ]
         }
       }   
