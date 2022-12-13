681547bb5ea89da4c7c6593ab17c8ebaf95da58e
ncruz93 <nickolas438@gmail.com>
2022-12-13 01:22:12 -0500

    added copyright to footer, set up vue for nav

--- main.js
+++ main.js
@@ -1,47 +1,15 @@
 const {createApp} = Vue;
 
-const app = createApp(App);
-
 createApp({
     data() {
         return {
-            message: "hello"
+          items: [
+            { text: 'Home Page', link: "index.html" },
+            { text: 'Gallery', link:"gallery.html"},
+            { text: 'Hobbies Page', link: "hobbies.html" },
+            { text: 'Bio', link:"bio.html"},
+            { text: 'Contact Me', link:"contact.html"}
+        ]
         }
-    }
-}).mount("#app")
-// createApp({
-//     data() {
-//         return {
-//           items: [
-//             { text: 'Home', link: "index.html" },
-//             { text: 'Gallery', link:"gallery.html"}
-//         ]
-//         }
-//       }   
-// }).mount("#navBar")
-
-
-// createApp({
-//     data() {
-//         return {
-//           sources:[
-//             'images/Suvi_on_hike.jpg',
-//             'images/Luna_toy.jpg',
-//             'images/Suvi_smiling.jpg',
-//             'images/Luna_Snow.jpg',
-//             'images/dogs_in_a_box.jpg',
-//             'images/Luna_Snow_Landscape.jpg',
-//             'images/Maui_Sunrise.jpg',
-//             'Images/beach.jpg',
-//             'images/img9.jpgimages/Luna_bed.jpg',
-//             'Images/Suvi_Luna.jpg',
-//             'images/Suvi_hikeHop.jpg'
-//           ]
-//         }
-//       },
-//       methods: {
-//         show_current_source: data(source){
-//           console.log(`Current Source: ${source}`)
-//         }
-//       }
-// })
+      }   
+}).mount("#navBar")
